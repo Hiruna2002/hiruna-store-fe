2ed209696f6f8bb54040bb486ed22c299533a69d
unknown <hirunahansindu123@gmail.com>
2025-12-02 18:50:09 +0530

    first commit

---
+++ src/components/Layout/UserLayout.tsx
@@ -0,0 +1,11 @@
+import Header from "../Common/Header"
+
+const UserLayout: React.FC = () => {
+    return(
+        <>
+            <Header />
+        </>
+    );  
+};
+
+export default UserLayout
