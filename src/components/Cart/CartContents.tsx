7dbcdfbbfc5272cdc679751d0ee16144111a1f27
unknown <hirunahansindu123@gmail.com>
2025-12-09 20:31:03 +0530

    finished design cart and nav bar

--- src/components/Cart/CartContents.tsx
+++ src/components/Cart/CartContents.tsx
@@ -1,4 +1,64 @@
+import React from "react";
+import { RiDeleteBin3Line } from "react-icons/ri";
+
+interface CartProduct {
+    productId: number;
+    name: string;
+    size: string;
+    color: string;
+    quantity: number;
+    price: number;
+    image: string;
+}
+
 const CartContents: React.FC =()=> {
-    return <div></div>;
+    const cartProducts: CartProduct[] = [
+        {
+            productId: 1,
+            name:"T-Shirt",
+            size: "M",
+            color: "Red",
+            quantity: 1,
+            price: 1000,
+            image: "https://picsum.photos/200?random=1"
+        },
+        {
+            productId: 2,
+            name:"Jeans",
+            size: "L",
+            color: "Blue",
+            quantity: 1,
+            price: 3500,
+            image: "https://picsum.photos/200?random=2"
+        }
+    ];
+    return (
+        <div>
+            {cartProducts.map((product, index) => (
+                <div key={index} className="flex items-start justify-between py-4 border-b">
+                    <div className="flex items-start">
+                        <img src={product.image} alt={product.name} className="w-20 h-24 object-cover mr-4 rounded"/>
+                        <div>
+                            <h3>{product.name}</h3>
+                            <p className="text-sm text-gray-500">
+                                size: {product.size} | color: {product.color}
+                            </p>
+                            <div className="flex items-center mt-2">
+                                <button className="border rounded px-2 py-1 text-xl font-medium">-</button>
+                                <span className="mx-4">{product.quantity}</span>
+                                <button className="border rounded px-2 py-1 text-xl font-medium">+</button>
+                            </div>
+                        </div>
+                    </div>
+                    <div>
+                        <p>Rs. {product.price.toLocaleString()}</p>
+                        <button>
+                            <RiDeleteBin3Line className="w-6 h-6 mt-2 text-red-600" />
+                        </button>
+                    </div>
+                </div>
+            ))}
+        </div>
+    );
 };
 export default CartContents;
--- src/components/Common/Navbar.tsx
+++ src/components/Common/Navbar.tsx
@@ -3,13 +3,19 @@ import { HiOutlineUser, HiOutlineShoppingBag, HiBars3BottomRight } from "react-i
 import SearchBar from "./Searchbar";
 import CartDrawer from "../Layout/CartDrawer";
 import React, { useState } from "react"; 
+import { IoMdClose } from "react-icons/io";
 
 const Navbar: React.FC = () => {
 
     const [drawerOpen, setDrawerOpoen] = useState<boolean>(false)
+    const [navDrawerOpen, setNavDrawOpen] = useState<boolean>(false)
+
+    const toggleNavDrawer = () => {
+        setNavDrawOpen(!navDrawerOpen);
+    }
 
     const toggleCartDrawer = () => {
-        setDrawerOpoen(!drawerOpen)
+        setDrawerOpoen(!drawerOpen);
     };
 
     return (
@@ -55,12 +61,44 @@ const Navbar: React.FC = () => {
                 <div className="overflow-hidden">
                     <SearchBar />
                 </div>
-                <button className="md:hidden">
+                <button onClick={toggleNavDrawer} className="md:hidden">
                     <HiBars3BottomRight className="h-6 w-6 text-gray-700" />
                 </button>
                 </div>
             </nav>
             <CartDrawer drawerOpen={drawerOpen} toggleCartDrawer={toggleCartDrawer}/>
+
+            {/* {Mobile Navigation} */}
+            <div className={`fixed top-0 left-0 w-3/4 sm:w-1/2 md:w-1/3 h-full bg-white shadow-lg 
+            transform transition-transform duration-300 z-50 ${navDrawerOpen ? "translate-x-0" : 
+            "-translate-x-full"}`}>
+                <div className="flex justify-end p-4">
+                    <button onClick={toggleNavDrawer}>
+                        <IoMdClose className="h-6 w-6 text-gray-600" />
+                    </button>
+                </div>
+                <div className="p-4">
+                    <h2 className="text-xl font-semibold mb-4">Menu</h2>
+                    <nav className="space-y-4">
+                        <Link to="#" onClick={toggleNavDrawer} 
+                        className="block text-gray-600 hover:text-black">
+                            Men
+                        </Link>
+                        <Link to="#" onClick={toggleNavDrawer} 
+                        className="block text-gray-600 hover:text-black">
+                            Women
+                        </Link>
+                        <Link to="#" onClick={toggleNavDrawer} 
+                        className="block text-gray-600 hover:text-black">
+                            Top Wear
+                        </Link>
+                        <Link to="#" onClick={toggleNavDrawer} 
+                        className="block text-gray-600 hover:text-black">
+                            Bottom Wear
+                        </Link>
+                    </nav>
+                </div>
+            </div>
         </>
     );
 };
