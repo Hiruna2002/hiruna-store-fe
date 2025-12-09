845910f5fc30327a5b54b2faafc587b22f5ed24d
unknown <hirunahansindu123@gmail.com>
2025-12-09 21:21:19 +0530

    finished design footer section

--- src/components/Layout/UserLayout.tsx
+++ src/components/Layout/UserLayout.tsx
@@ -1,9 +1,11 @@
 import Header from "../Common/Header"
+import Footer from "../Common/Footer";
 
 const UserLayout: React.FC = () => {
     return(
         <>
             <Header />
+            <Footer />
         </>
     );  
 };
